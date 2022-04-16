1e148455711e22de5b2a48bcf7cacf3a9a39a7f2
Amelie Pira <amelie.pira@gmail.com>
2022-04-16 16:52:40 +0100

    added draft pseudo code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,59 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+const getPasswordLength = () => {
+  if () {
+    if () {
+    } else {
+    }
+  } else {
+  }
+  return;
+};
+
+const getPasswordOptions = () => {
+  if () {
+    if () {
+    } else {
+    }
+  } else {
+  }
+  return;
+};
+
+const createPassword = () => {
+  for () {
+    
+  }
+  
+  for () {
+    
+  }
+  
+};
+
+const shufflePassword = () => {
+  //shuffle function
+  return;
+  
+};
+
 // main function to generate the random password
 const generatePassword = () => {
-  /*
-   * All your code goes here to generate a password
-   */
+  //All the logic for the generatePassword function is here
+
+  //get the password length from the user
+  //prompt to the user to enter the password length
+  //check if input is a number
+  const passwordLength = getPasswordLength();
+
+  //get the password options from the user
+  const chosenOptions = getPasswordOptions();
+  
+  //create the password
+  const tempPassword = createPassword(passwordLength, chosenOptions);
+  const randomPassword = shufflePassword(tempPassword);
+  return randomPassword;
 };
 
 // Write password to the #password input
